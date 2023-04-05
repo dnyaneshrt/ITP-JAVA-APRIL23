208b8e7f79d037fe7b61fd565ad7a4e121bbf99f
Dnyaneshwar <sartape51@gmail.com>
2023-04-05 20:08:34 +0530

    initial commit

---
+++ src/basic_programs/First.java
@@ -0,0 +1,9 @@
+package basic_programs;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Welcome C Developers!");
+        System.out.println("Welcome Android Developers!");
+        System.out.println("Welcome Java Developers!");
+    }
+}
